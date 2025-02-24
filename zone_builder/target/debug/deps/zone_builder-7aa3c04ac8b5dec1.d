3d1531129e336b1f58c7aa2818796897078d9fc3
黄炜杰 <wjhuang@live.cn>
2025-02-24 12:31:38 +0800

    删除无用文件

--- zone_builder/target/debug/deps/zone_builder-7aa3c04ac8b5dec1.d
+++
@@ -1,5 +0,0 @@
-/Users/GHuang/WorkSpace/ArticleProjects/hwj-zone-articles/zone_builder/target/debug/deps/libzone_builder-7aa3c04ac8b5dec1.rmeta: src/main.rs
-
-/Users/GHuang/WorkSpace/ArticleProjects/hwj-zone-articles/zone_builder/target/debug/deps/zone_builder-7aa3c04ac8b5dec1.d: src/main.rs
-
-src/main.rs:
--- zone_builder/target/debug/deps/zone_builder-d8ee99a14198ead2.d
+++
@@ -1,5 +0,0 @@
-/Users/GHuang/WorkSpace/ArticleProjects/hwj-zone-articles/zone_builder/target/debug/deps/libzone_builder-d8ee99a14198ead2.rmeta: src/main.rs
-
-/Users/GHuang/WorkSpace/ArticleProjects/hwj-zone-articles/zone_builder/target/debug/deps/zone_builder-d8ee99a14198ead2.d: src/main.rs
-
-src/main.rs:
